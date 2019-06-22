acf92a1071b50c1b9e5e2dd473e0a88caf7eca2f
matthew rich <rich.matthewJ@gmail.com>
2019-06-22 18:22:39 -0500

    handling connection issues

--- src/pages/Register-page.vue
+++ src/pages/Register-page.vue
@@ -1,18 +1,21 @@
 <template>
     <div>
-        <form id="registerSection" v-if="!searching && !playerFound" v-on:submit.prevent>
+        <form id="registerSection" v-if="!searching && !playerFound && !showConnectionError"  v-on:submit.prevent>
             <h1>Please enter a username</h1>
             <input type="text" id="name" v-model="name">
             <br>
             <button @click="searchForGame()">Search for game</button>
         </form>
         <div id="searchingLoader" v-if="searching">Searching for a game....</div>
-        <div id="playerFound" v-if="playerFound">You have matched with {{ opponent }}</div>
+        <div id="playerFound" v-if="playerFound">You have matched with {{ opponent.name }}</div>
+        <div id="connectionError" v-if="showConnectionError" style="color: red">We ran into a problem connecting you to a match</div>
+
     </div>
 </template>
 
 <script>
-import socket from '../socket';
+import io from 'socket.io-client';
+
 
 export default {
   name: 'Register-page',
@@ -21,24 +24,49 @@ export default {
   },
   data() {
     return {
+      socket: '',  
       searching: false,
       name: '',
       playerFound: false,
-      opponent: ''
+      opponent: '',
+      showConnectionError: false
     }
-  },
-  mounted() {
-    socket.on("opponent-found", (opponent) => {
-      this.searching = false;
-      this.opponent = opponent
-      this.playerFound = true;
-    })
-
   },
   methods: {
     searchForGame() {
-      socket.emit("game-searching", this.name);
-      this.searching = true;
+            this.socket = io('http://localhost:3500', {
+                 reconnection: false
+            });
+
+
+            this.socket.on("connect_error", () => {
+                this.showConnectionError = true;
+            })
+
+            this.socket.on("connect", () => {
+                this.socket.on("opponent-found", (opponent) => {
+                this.searching = false;
+                this.opponent = opponent
+                this.playerFound = true;
+                this.socket.disconnect();
+                const nameSpaceSocket = io('http://localhost:3500' + opponent.nameSpace);
+
+                nameSpaceSocket.on("welcome", (data) => {
+                    alert("weclome to namespace " + data)
+                })
+
+                nameSpaceSocket.on("user-left", () => {
+                    alert("USER HAS DISCONNECTED")
+                })
+                })
+                this.socket.emit("game-searching", this.name);
+                this.searching = true;
+            })
+
+            
+
+
+   
     }
   }
 }
