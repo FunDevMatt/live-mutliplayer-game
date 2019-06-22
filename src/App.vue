75af5bfd2fcfd28b0791d14b73ad5867fda82b0e
matthew rich <rich.matthewJ@gmail.com>
2019-06-22 16:29:06 -0500

    handling player connections

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,56 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <form id="registerSection" v-if="!searching && !playerFound" v-on:submit.prevent>
+      <h1>Please enter a username</h1>
+      <input type="text" id="name" v-model="name">
+      <br>
+      <button @click="searchForGame()">Search for game</button>
+    </form>
+    <div id="searchingLoader" v-if="searching">Searching for a game....</div>
+    <div id="playerFound" v-if="playerFound">You have matched with {{ opponent }}</div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import socket from './socket';
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      searching: false,
+      name: '',
+      playerFound: false,
+      opponent: ''
+    }
+  },
+  mounted() {
+    socket.on("opponent-found", (opponent) => {
+      this.searching = false;
+      this.opponent = opponent.name;
+      this.playerFound = true;
+    })
+
+  },
+  methods: {
+    searchForGame() {
+      socket.emit("game-searching", this.name);
+      this.searching = true;
+    }
   }
 }
 </script>
 
 <style>
-#app {
+* {
+  text-align: center;
+  padding: 0;
+  margin: 0;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
